089ebeb00e13c88d93b29d2c58d93db9cae5b0e2
karimhafiz <hafiz_2010@outlook.com>
2025-04-27 19:15:55 +0100

    adjusted index for vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const connectDB = require("./config/db");
+const connectDB = require("../config/db");
 connectDB();
 
 const cors = require("cors");
@@ -13,22 +13,19 @@ app.use(cors());
 // Serve static files from the "uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const eventRoutes = require("./routes/events");
-const ticketRoutes = require("./routes/tickets");
-const paymentRoutes = require("./routes/payments");
-const adminRoutes = require("./routes/admin");
+const eventRoutes = require("../routes/events");
+const ticketRoutes = require("../routes/tickets");
+const paymentRoutes = require("../routes/payments");
+const adminRoutes = require("../routes/admin");
 
 app.use("/api/payments", paymentRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/tickets", ticketRoutes);
-app.use("/api/admins", adminRoutes); //
-
-const PORT = process.env.PORT || 5000;
+app.use("/api/admins", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Event Ticketing API is running...");
 });
 
-app.listen(PORT, "192.168.1.116", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export the app for Vercel serverless
+module.exports = app;
