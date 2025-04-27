43c9576d68981864bf9293810251f900fe42c86b
karimhafiz <hafiz_2010@outlook.com>
2025-04-27 23:34:11 +0100

    add config/db.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const connectDB = require("../config/db");
+const connectDB = require("./config/db"); // Import the database connection function
 connectDB();
 
 const cors = require("cors");
@@ -13,10 +13,10 @@ app.use(cors());
 // Serve static files from the "uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const eventRoutes = require("../routes/events");
-const ticketRoutes = require("../routes/tickets");
-const paymentRoutes = require("../routes/payments");
-const adminRoutes = require("../routes/admin");
+const eventRoutes = require("./routes/events");
+const ticketRoutes = require("./routes/tickets");
+const paymentRoutes = require("./routes/payments");
+const adminRoutes = require("./routes/admin");
 
 app.use("/payments", paymentRoutes);
 app.use("/events", eventRoutes);
