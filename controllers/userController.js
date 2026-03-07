90a213511c4bedf99e6137f143f4c08b972ed7f4
unknown <eliteyasin1@gmail.com>
2026-03-08 01:34:30 +0500

    added error checks to see if the email the user entered is connected with a logged in google account

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
-const Ticket = require("../models/Ticket");  
-const Team   = require("../models/Team"); 
+const Ticket = require("../models/Ticket");
+const Team = require("../models/Team");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -11,7 +11,12 @@ exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "Email already in use" });
+      if (!existingUser.password) {
+        return res.status(400).json({
+          message: "This email is linked to a Google account. Please log in with Google.",
+        });
+      }
+      return res.status(400).json({ message: "Email already in use." });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword, role: "user" });
